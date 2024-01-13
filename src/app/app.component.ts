7a7564740614317e99d3540affb8715d5917b5ec
Samuelmwaniki <smwaniki549@gmail.com>
2024-01-13 11:23:42 +0300

    installing angular materials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCommonModule } from '@angular/material/core';
+import { MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatSlideToggleModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
